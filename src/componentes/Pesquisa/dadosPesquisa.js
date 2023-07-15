0b869b3b3c854440d8de93369ac8f6b4d32147b1
Kodaac <nogueira.hs@hotmail.com>
2023-07-14 23:27:51 -0300

    Ação "Pesquisar" funcionando

---
+++ src/componentes/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,9 @@
+import livro from '../../imagens/livro.png';
+
+export const livros = [
+    {nome: 'Liderança em Design', id: 1, src: livro},
+    {nome: 'Javascript Assertivo', id: 2, src: livro},
+    {nome: 'PHP Poo', id: 3, src: livro},
+    {nome: 'Pentest', id: 4, src: livro},
+    {nome: 'C# Avançado', id: 5, src: livro},
+]
